be1e8924b7423dd732c7a6c0346236af936a2be3
blacketde <huy0367966@gmail.com>
2023-06-02 11:18:41 +0700

    diem danh tuan 5

--- BigSchool/Models/AccountViewModels.cs
+++ BigSchool/Models/AccountViewModels.cs
@@ -79,10 +79,12 @@ namespace BigSchool.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
 
+        
+        public string ConfirmPassword { get; set; }
         [Required]
         [StringLength(255)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
-        public string ConfirmPassword { get; set; }
     }
 
     public class ResetPasswordViewModel
